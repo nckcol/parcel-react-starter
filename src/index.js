80b662b49bc231d380c1a97c736b19cbeaf29bab
Nikita Popov <nckcol@gmail.com>
2018-10-07 15:10:45 +0300

    style: remove spaces

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
+import React, {Component} from 'react';
+import {render} from 'react-dom';
 import styled from 'astroturf';
 
 const Title = styled('h1')`
